2fb8da38eedc5c13d069b811a074ddce2f5aeda1
Henish Borad <henishborad@gmail.com>
2024-01-08 22:56:50 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+using namespace std;
+/*
+ * a = 1
+ * b = 2
+ *
+ * sum = a + b
+ * a = b
+ * b = sum
+ *
+ * */
 
 int main() {
-    std::cout << "Hari Om" << std::endl;
+    int a = 1;
+
+    int b = 1;
+
+    int counter = 1;
+    int sum = 0;
+    int how_many;
+    cout << "how many numbers?";
+    cin >> how_many;
+    cout << a << " ";
+    cout << b << " ";
+
+    while(counter != how_many) {
+        sum = a + b;
+        cout << sum << " ";
+        a = b;
+        b = sum;
+        counter++;
+    }
+
     return 0;
 }
