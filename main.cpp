3b46abf48c926b0bc99a4b537813d67dfe4ca13d
Henish Borad <henishborad@gmail.com>
2024-01-08 22:58:58 -0800

    update

--- main.cpp
+++ main.cpp
@@ -2,25 +2,24 @@
 using namespace std;
 
 int main() {
-    int a = 1;
-
-    int b = 1;
-
-    int counter = 1;
-    int sum = 0;
-    int how_many;
-    cout << "how many numbers?";
-    cin >> how_many;
-    cout << a << " ";
-    cout << b << " ";
-
-    while(counter != how_many) {
-        sum = a + b;
-        cout << sum << " ";
-        a = b;
-        b = sum;
-        counter++;
-    }
 
     return 0;
 }
+
+//fibonacci
+//int a = 1;
+//int b = 1;
+//int counter = 1;
+//int sum = 0;
+//int how_many;
+//cout << "how many numbers?";
+//cin >> how_many;
+//cout << a << " ";
+//cout << b << " ";
+//while(counter != how_many) {
+//sum = a + b;
+//cout << sum << " ";
+//a = b;
+//b = sum;
+//counter++;
+//}
