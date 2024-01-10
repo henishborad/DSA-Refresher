480fc26b4d714db6954ea7bc9c080f5a96c20612
Henish Borad <henishborad@gmail.com>
2024-01-10 13:15:02 -0800

    update

--- main.cpp
+++ main.cpp
@@ -2,27 +2,38 @@
 using namespace std;
 
 int main() {
-    int input;
-    cout << "Enter the number to check if prime or not:";
-    cin >> input;
-    int divisor = 2;
-    bool isPrime = true;
-    while (divisor != input-1) {
-        if (input % divisor != 0) {
-            divisor++;
+    int i = 0;
+    while (i < 3) {
+        int j = 0;
+        while (j < 3) {
+            cout << "* ";
+            j++;
         }
-        else {
-            cout << "Input is not a prime number.";
-            isPrime = false;
-            break;
-        }
-    }
-    if (isPrime) {
-        cout << "Prime input!";
+        cout << endl;
+        i++;
     }
     return 0;
 }
 
+//prime
+//int input;
+//cout << "Enter the number to check if prime or not:";
+//cin >> input;
+//int divisor = 2;
+//bool isPrime = true;
+//while (divisor != input-1) {
+//if (input % divisor != 0) {
+//divisor++;
+//}
+//else {
+//cout << "Input is not a prime number.";
+//isPrime = false;
+//break;
+//}
+//}
+//if (isPrime) {
+//cout << "Prime input!";
+//}
 //fibonacci
 //int a = 1;
 //int b = 1;
