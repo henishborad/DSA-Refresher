9fcc4d61ff3a344e1e2b70a023a7835075c9073f
Henish Borad <henishborad@gmail.com>
2024-01-14 12:30:22 -0800

    update

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hari Om")
+def factorial(n):
+    # Base case
+    if n == 0 or n == 1:
+        return 1
+    # Recursive case
+    else:
+        return n * factorial(n - 1)
+
+
+# Testing the factorial function
+result = factorial(5)
+print(result)  # Output: 120
