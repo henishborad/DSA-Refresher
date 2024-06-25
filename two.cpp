cf0b073a12a69c342af08320c0b27ec25a36221c
Henish Borad <henishbrb@gmail.com>
2024-06-24 20:13:03 -0700

    Update

--- two.cpp
+++ two.cpp
@@ -1,11 +1,11 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int b_search(int arr[], int n, int target)
+int firstAndLast(int arr[], int n, int target)
 {
     int start = 0;
     int end = n - 1;
-
+    int ans = -1;
     while (start <= end)
     {
         int mid = start + (end - start) / 2;
@@ -15,6 +15,7 @@ int b_search(int arr[], int n, int target)
         }
         else if (arr[mid] > target)
         {
+            ans = mid;
             end = mid - 1;
         }
         else
@@ -22,12 +23,12 @@ int b_search(int arr[], int n, int target)
             start = mid + 1;
         }
     }
-    return -1; // Return -1 if the target is not found
+    return ans;
 }
 
 int main()
 {
-    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int n = sizeof(arr) / sizeof(arr[0]); // Calculate the size of the array
-    cout << b_search(arr, n, 5) << endl;
+    int arr[10] = {0, 1, 2, 5, 5, 6, 8, 8, 8, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << firstAndLast(arr, n, 9);
 }
---
+++ zero.cpp
@@ -0,0 +1,10 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+int main()
+{
+    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << n;
+}
