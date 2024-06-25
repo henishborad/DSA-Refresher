9149a892b162ad72d7f2de7bbd43d7dae7f2fe9c
Henish Borad <henishbrb@gmail.com>
2024-06-24 20:57:56 -0700

    update

--- zero.cpp
+++ zero.cpp
@@ -1,10 +1,31 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+int peakIndex(int arr[], int n)
+{
+    int start = 0;
+    int end = n - 1;
+    int peak_index = n;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] > arr[mid - 1])
+        {
+            peak_index = mid;
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return arr[peak_index];
+}
 
 int main()
 {
-    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout << n;
+    int arr[10] = {0, 1, 2, 3, 4, 4, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << peakIndex(arr, n);
 }
