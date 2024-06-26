f9eaa9940c3c52b2bd655bb555b2ff0fd756883b
Henish Borad <henishbrb@gmail.com>
2024-06-25 22:24:30 -0700

    update

---
+++ app.py
@@ -0,0 +1,7 @@
+arr = [1, 4, 6, 8, 9, 6, 5, 3]
+
+start = 0
+end = len(arr)-1
+
+while(start<=end):
+    
--- one.cpp
+++ one.cpp
@@ -1,6 +1,21 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int n;
+    int i = 0;
+    cin >> n;
+    vector<int> v(n);
+    while (i < n)
+    {
+        int j;
+        cin >> j;
+        v.push_back(j);
+        i++;
+    }
+    for (int i : v)
+    {
+        cout << i;
+    }
 }
