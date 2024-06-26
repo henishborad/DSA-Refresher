7c4e7eaa6816db3f616b2c04a1f7ac0633404d25
Henish Borad <henishbrb@gmail.com>
2024-06-25 21:22:46 -0700

    update

--- one.cpp
+++ one.cpp
@@ -1,33 +1,6 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int b_search(int arr[], int n, int target)
-{
-    int start = 0;
-    int end = n - 1;
-
-    while (start <= end)
-    {
-        int mid = start + (end - start) / 2;
-        if (arr[mid] == target)
-        {
-            return mid;
-        }
-        else if (arr[mid] > target)
-        {
-            end = mid - 1;
-        }
-        else
-        {
-            start = mid + 1;
-        }
-    }
-    return -1; // Return -1 if the target is not found
-}
-
-int main()
-{
-    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int n = sizeof(arr) / sizeof(arr[0]); // Calculate the size of the array
-    cout << b_search(arr, n, 5) << endl;
+int main(){
+    
 }
